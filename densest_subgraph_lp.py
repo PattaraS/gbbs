09227620248cb5d3327dde304de394f74a858002
Pat Sukprasert <pattara.sk127@gmail.com>
2022-10-08 19:15:16 -0500

    Make LP a function

--- densest_subgraph_lp.py
+++ densest_subgraph_lp.py
@@ -20,37 +20,48 @@ adj_list = [
     (8,9)
 ]
 
-m = gp.Model()
+def densest_subgraph_lp_run(adj_list):
+    m = gp.Model()
 
-grb_type = GRB.CONTINUOUS
+    grb_type = GRB.CONTINUOUS
 
-vertex_vars = dict()
-edge_vars = list()
-for edge in adj_list:
-    u = edge[0]
-    v = edge[1]
-    if u not in vertex_vars:
-        vertex_vars[u] = m.addVar(lb=0, ub=1, name='vertex_{}'.format(u), vtype=grb_type)
-    if v not in vertex_vars:
-        vertex_vars[v] = m.addVar(lb=0, ub=1, name='vertex_{}'.format(v), vtype=grb_type)
-    if u > v:
-        u,v = v,u
-    edge_var = m.addVar(lb=0, ub=1, name='edge_{}_{}'.format(u,v), vtype=grb_type)
+    vertex_vars = dict()
+    edge_vars = list()
+    for edge in adj_list:
+        u = edge[0]
+        v = edge[1]
+        if u not in vertex_vars:
+            vertex_vars[u] = m.addVar(lb=0, ub=1, name='vertex_{}'.format(u), vtype=grb_type)
+        if v not in vertex_vars:
+            vertex_vars[v] = m.addVar(lb=0, ub=1, name='vertex_{}'.format(v), vtype=grb_type)
+        if u > v:
+            u,v = v,u
+        edge_var = m.addVar(lb=0, ub=1, name='edge_{}_{}'.format(u,v), vtype=grb_type)
 
-    m.addConstr(edge_var <= vertex_vars[u])
-    m.addConstr(edge_var <= vertex_vars[v])
-    edge_vars.append(edge_var)
+        m.addConstr(edge_var <= vertex_vars[u])
+        m.addConstr(edge_var <= vertex_vars[v])
+        edge_vars.append(edge_var)
 
-m.addConstr(quicksum(vertex_vars[u] for u in vertex_vars) <= 1)
+    m.addConstr(quicksum(vertex_vars[u] for u in vertex_vars) <= 1)
 
-m.setObjective(quicksum(e for e in edge_vars), GRB.MAXIMIZE)
+    m.setObjective(quicksum(e for e in edge_vars), GRB.MAXIMIZE)
 
-m.optimize()
+    m.optimize()
 
-print('Density: {}'.format(m.objVal))
+    print('Density: {}'.format(m.objVal))
 
-n = sum(1 for u in vertex_vars if vertex_vars[u].x > 0)
-m = sum(1 for e in edge_vars if e.x > 0 )
+    n = sum(1 for u in vertex_vars if vertex_vars[u].x > 0)
+    m = sum(1 for e in edge_vars if e.x > 0 )
 
-print('n: {}, m: {}'.format(n,m))
+    print('n: {}, m: {}'.format(n,m))
+
+def read_adj_list_file(name):
+    adj_list = list()
+    with open(name, 'r') as f:
+        for line in f.readlines():
+            tokens = line.split('\t')
+            u = int(tokens[0])
+            v = int(tokens[1])
+            adj_list.append((u,v))
+    return adj_list
 
